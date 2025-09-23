6eb1e527ba626ec1c4729ba589a580ff545908de
LinSaki <saquika@sheridancollege.ca>
2025-09-23 13:58:13 -0400

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { photagenix } from "../assets";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-border-n-6 lg:bg-n-8/90 lg:backdrop-blue-sm-">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={photagenix} width={190} height={40} alt="Photagenix" />
+        </a>
+        <nav></nav>
+      </div>
+      Header
+    </div>
+  );
+};
+
+export default Header;
