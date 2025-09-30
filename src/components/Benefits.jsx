ff739a97f5e0f813371d0a1e1b09c63eed453334
LinSaki <saquika@sheridancollege.ca>
2025-09-30 15:34:14 -0400

    Created Benefits and Heading components

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Benefits of DiffQuest"
+        />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
